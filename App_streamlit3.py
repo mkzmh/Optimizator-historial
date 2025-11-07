33c130647f5036494772022fdc9b0bfb422a33a2
mkzmh <martinakozameh61@gmail.com>
2025-11-07 10:37:23 -0300

    Update App_streamlit3.py

--- App_streamlit3.py
+++ App_streamlit3.py
@@ -7,7 +7,7 @@ import os
 import time
 
 # 💡 IMPORTACIÓN: Usar el nombre de archivo exacto que tienes en GitHub
-from Routing_logic3 import COORDENADAS_LOTES, solve_route_optimization, VEHICLES, COORDENADAS_ORIGEN
+from logic_core import COORDENADAS_LOTES, solve_route_optimization, VEHICLES, COORDENADAS_ORIGEN
 # --- FUNCIÓN DE GOOGLE MAPS MOVIDA AL ARCHIVO PRINCIPAL PARA EVITAR ERRORES DE IMPORTACIÓN ---
 def generate_google_maps_link(optimized_coords_sequence):
     """
@@ -360,5 +360,6 @@ elif page == "Estadísticas":
 
     else:
         st.info("No hay datos en el historial para generar estadísticas.")
+
 
 
