eb695a035509d9bd86f10a3e84bac66ad1d3b47e
mkzmh <martinakozameh61@gmail.com>
2025-11-07 08:19:59 -0300

    Update App_streamlit3.py

--- App_streamlit3.py
+++ App_streamlit3.py
@@ -7,7 +7,7 @@ import json
 import gspread # Necesario para la conexión a Google Sheets
 
 # Importa la lógica y constantes del módulo vecino (Asegúrate que se llama 'routing_logic.py')
-from routing_logic import COORDENADAS_LOTES, solve_route_optimization, VEHICLES, COORDENADAS_ORIGEN 
+from Routing_logic3 import COORDENADAS_LOTES, solve_route_optimization, VEHICLES, COORDENADAS_ORIGEN 
 
 # =============================================================================
 # CONFIGURACIÓN INICIAL Y PERSISTENCIA DE DATOS (GOOGLE SHEETS)
@@ -333,3 +333,4 @@ elif page == "Estadísticas":
 
     else:
         st.info("No hay datos en el historial para generar estadísticas.")
+
