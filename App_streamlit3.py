8d6bbfbe78e677f13567272e9d0129ecb3de9ac6
mkzmh <martinakozameh61@gmail.com>
2025-11-07 10:55:06 -0300

    Update App_streamlit3.py

--- App_streamlit3.py
+++ App_streamlit3.py
@@ -299,9 +299,10 @@ elif page == "Historial":
                          "Hora": "Hora de Carga", # Nombre visible en Streamlit
                          "Lotes_ingresados": "Lotes Ingresados"
                      })    
-    else:
+else:
         st.info("No hay rutas guardadas. Realice un cálculo en la página principal.")
-    else:
+else:
         st.info("No hay datos en el historial para generar estadísticas.")
+
 
 
