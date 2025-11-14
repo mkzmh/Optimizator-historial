317b06458b3dcbe49b153c934e65d489c4ff535b
mkzmh <martinakozameh61@gmail.com>
2025-11-14 10:22:46 -0300

    Update App_streamlit3.py

--- App_streamlit3.py
+++ App_streamlit3.py
@@ -568,4 +568,5 @@ elif page == "Estadísticas":
             )
         
         st.divider()
-        st.caption("Nota: Los KM Totales/Promedio se calculan usando la suma de las distancias optimizadas de cada camión."
+        st.caption("Nota: Los KM Totales/Promedio se calculan usando la suma de las distancias optimizadas de cada camión.")
+
